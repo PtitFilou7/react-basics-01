6848521d6d37a2c5b1bef2691e3cf2292fbab58f
PtitFilou7 <remi.karleskind21@gmail.com>
2025-10-21 15:04:42 +0200

    react basics 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
+import "./App.css";
 
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
-  return (
-    <div>
-		<PokemonCard/>
-	</div>
-  );
+	return (
+		<div>
+			<PokemonCard />
+		</div>
+	);
 }
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,32 @@
-function PokemonCard() {
-  return (
-  <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-    <figcaption>Bulbasaur</figcaption>
-  </figure>)
-  
-}
+// function PokemonCard() {
+// 	return (
+// 		<figure>
+// 			<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur"/>
+// 			<figcaption>Bulbasaur</figcaption>
+// 		</figure>
+// 	);
+// }
 
-export default PokemonCard ;
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
+];
+
+function PokemonCard() {
+	const pokemon = pokemonList[0];
+
+	return (
+		<>
+			{pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} alt={pokemon.name} />}
+			<figcaption>{pokemon.name}</figcaption>
+
+		</>
+	);
+}
+export default PokemonCard;
