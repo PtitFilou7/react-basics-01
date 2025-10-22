4dd48974178935520091024ccc3c1d2f6f7f2e21
PtitFilou7 <remi.karleskind21@gmail.com>
2025-10-22 11:41:47 +0200

    quêtes4

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,32 +1,14 @@
-// function PokemonCard() {
-// 	return (
-// 		<figure>
-// 			<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur"/>
-// 			<figcaption>Bulbasaur</figcaption>
-// 		</figure>
-// 	);
-// }
-
-const pokemonList = [
-	{
-		name: "bulbasaur",
-		imgSrc:
-			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-	},
-	{
-		name: "mew",
-	},
-];
-
-function PokemonCard() {
-	const pokemon = pokemonList[0];
-
+function PokemonCard({ pokemon }) {
 	return (
 		<>
-			{pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} alt={pokemon.name} />}
+			{pokemon.imgSrc === undefined ? (
+				<p>???</p>
+			) : (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			)}
 			<figcaption>{pokemon.name}</figcaption>
-
 		</>
 	);
 }
+
 export default PokemonCard;
